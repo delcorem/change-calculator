a6661d179cba793501c1d44f51bbe4dd5c624e36
Michael Delcore <delcorem@sheridancollege.ca>
2021-05-30 21:48:27 -0400

    adding initial files

---
+++ changeUtil.c
@@ -0,0 +1,7 @@
+#include "changeUtil.h"
+
+//Calculates the change in terms of dollars, quarters, dimes, nickels, and pennies
+// 'moneyOwed' is --------, 'moneyPaid' is --------, and 'change' is -------
+void calcChange(moneyOwed, moneyPaid, change) {
+	
+}
---
+++ mainFile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "changeUtil.h"
+
+// Author: Michael Delcore
+int main(int argc, char** argv) {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
